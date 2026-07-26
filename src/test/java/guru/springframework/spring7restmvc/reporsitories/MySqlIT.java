1b8b87e9e7e0a155e8e9957c51371fdd150bde32
Obyat <rahman.obyat@gmail.com>
2026-07-26 11:53:05 -0400

    Chapter 13 docker compose and serviceconnection for IT

---
+++ src/test/java/guru/springframework/spring7restmvc/reporsitories/MySqlIT.java
@@ -0,0 +1,52 @@
+package guru.springframework.spring7restmvc.reporsitories;
+
+import guru.springframework.spring7restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("localmysql")
+public class MySqlIT {
+
+    @Container
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:9");
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+    BeerRepository beerRepository;
+
+
+    @DynamicPropertySource
+    static void mySqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+    }
+
+
+    @Test
+    void testListBeers() {
+        System.out.println("I'm here");
+        System.out.println("DataSource: " + dataSource);
+        System.out.println("BeerRepository: " + beerRepository);
+
+        List<Beer> beers = beerRepository.findAll();
+
+        assertThat(beers.size(), greaterThan(0));
+    }
+}
---
+++ src/test/java/guru/springframework/spring7restmvc/reporsitories/MySqlServiceConnectionIT.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring7restmvc.reporsitories;
+
+import guru.springframework.spring7restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("localmysql")
+public class MySqlServiceConnectionIT {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:9");
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+
+    @Test
+    void testListBeers() {
+        System.out.println("I'm here");
+        System.out.println("DataSource: " + dataSource);
+        System.out.println("BeerRepository: " + beerRepository);
+
+        List<Beer> beers = beerRepository.findAll();
+
+        assertThat(beers.size(), greaterThan(0));
+    }
+}
